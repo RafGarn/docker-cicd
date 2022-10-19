b5707a5237dcf1797942012ad3103bf56d03c831
RafGarn <98810962+RafGarn@users.noreply.github.com>
2022-10-19 11:17:55 +0200

    Update nodejs.groovy

--- basics/dsl/nodejs.groovy
+++ basics/dsl/nodejs.groovy
@@ -19,7 +19,7 @@ job('NodeJS example') {
 
 job('Raf example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git(https://github.com/RafGarn/docker-cicd.git) {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL yaniv')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
@@ -27,12 +27,9 @@ job('Raf example') {
     triggers {
         scm('H/5 * * * *')
     }
-    wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
-    }
+ 
     steps {
-        shell("docker run -t test:test .")
+        shell("docker build -t test:test .")
     }
 }
 
